b2610573f34b0b1dae3ce85a846afad298a89e82
Guilhermo González <guilhermo.gonzalez@grupoboticario.com.br>
2021-05-20 20:08:48 -0300

    Skeleton of users screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>DashGo</h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex 
+        as="form" 
+        w="100%" 
+        maxWidth={360} 
+        bg="gray.800" 
+        padding="8" 
+        borderRadius={8} 
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">Entrar</Button>
+        
+      </Flex>
+    </Flex>
   )
 }
